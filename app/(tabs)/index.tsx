8adea2d02a676b77aca38a636241c9c362875989
korkt-kim <gnscott6@naver.com>
2024-05-08 01:18:05 +0900

    feat: sitemap 생성

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { Stack } from 'expo-router';
+import { Link } from 'expo-router';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function TabOneScreen() {
   return (
@@ -9,6 +12,17 @@ export default function TabOneScreen() {
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Link href="/books/search" asChild>
+        <Pressable>
+          {({ pressed }) => (
+            <FontAwesome
+              name="info-circle"
+              size={25}
+              style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+            />
+          )}
+        </Pressable>
+      </Link>
     </View>
   );
 }
